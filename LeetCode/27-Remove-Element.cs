168c7907aea43992edcac4a026191e1a364625e3
Alvaro Croce <amcroce@outlook.com>
2024-11-27 23:35:41 -0300

    Remove element and refactor

---
+++ LeetCode/27-Remove-Element.cs
@@ -0,0 +1,133 @@
+﻿using NUnit.Framework;
+
+namespace LeetCode
+{
+    public class Remove_Element
+    {
+        public static int RemoveElement(int[] nums, int val)
+        {
+            var k = 0;
+            var lastPositionWithValue = nums.Length - 1;
+
+            for (int i = nums.Length - 1; i >= 0; i--)
+            {
+                if (nums[i] != val)
+                {
+                    k++;
+                    continue;
+                }
+
+                if (lastPositionWithValue > i)
+                {
+                    nums[i] = nums[lastPositionWithValue];
+                    nums[lastPositionWithValue] = val+1;
+                    lastPositionWithValue--;
+                }
+                else
+                {
+                    nums[i] = val+1;
+                    lastPositionWithValue = i-1;
+                }
+            }
+            return k;
+        }
+    }
+
+    // Generate a testing class for this method using NUnit
+    [TestFixture]
+    public class RemoveElementTests
+    {
+        private Remove_Element solution;
+        
+        [SetUp]
+        public void SetUp()
+        {
+            solution = new Remove_Element();
+        }
+
+        [Test]
+        public void Test1()
+        {
+            var nums = new int[] { 3, 2, 2, 3 };
+            var expectedArray = new int[] { 2, 2 };
+            var val = 3;
+            var expected = 2;
+            var result = Remove_Element.RemoveElement(nums, val);
+            Assert.That(result, Is.EqualTo(expected));
+            // Assert that first k values in nums contains all the numbers in the array except the val
+            for (int i = 0; i < expected; i++)
+            {
+                // Assert that expectedArray[i] is present in nums
+                Assert.That(nums.Take(expected).Contains(expectedArray[i]), Is.True);
+            }
+        }
+
+        [Test]
+        public void Test2()
+        {
+            var nums = new int[] { 0, 1, 2, 2, 3, 0, 4, 2 };
+            var expectedArray = new int[] { 0, 1, 3, 0, 4 };
+            var val = 2;
+            var expected = 5;
+            var result = Remove_Element.RemoveElement(nums, val);
+            Assert.That(result, Is.EqualTo(expected));
+            // Assert that first k values in nums contains all the numbers in the array except the val
+            for (int i = 0; i < expected; i++)
+            {
+                // Assert that expectedArray[i] is present in nums
+                Assert.That(nums.Take(expected).Contains(expectedArray[i]), Is.True);
+            }
+        }
+
+        [Test]
+        public void Test3()
+        {
+            var nums = new int[] { 0, 1, 2, 2, 3, 0, 2, 2 };
+            var expectedArray = new int[] { 0, 1, 3, 0 };
+            var val = 2;
+            var expected = 4;
+            var result = Remove_Element.RemoveElement(nums, val);
+            Assert.That(result, Is.EqualTo(expected));
+            // Assert that first k values in nums contains all the numbers in the array except the val
+            for (int i = 0; i < expected; i++)
+            {
+                // Assert that expectedArray[i] is present in nums
+                Assert.That(nums.Take(expected).Contains(expectedArray[i]), Is.True);
+            }
+        }
+
+        [Test]
+        public void Test4()
+        {
+            var nums = new int[] { 2 };
+            var expectedArray = new int[] { };
+            var val = 2;
+            var expected = 0;
+            var result = Remove_Element.RemoveElement(nums, val);
+            Assert.That(result, Is.EqualTo(expected));
+            // Assert that first k values in nums contains all the numbers in the array except the val
+            for (int i = 0; i < expected; i++)
+            {
+                // Assert that expectedArray[i] is present in nums
+                Assert.That(nums.Take(expected).Contains(expectedArray[i]), Is.True);
+            }
+        }
+
+        [Test]
+        public void Test5()
+        {
+            var nums = new int[] { 1, 2, 3, 4 };
+            var expectedArray = new int[] { 2, 3, 4 };
+            var val = 1;
+            var expected = 3;
+            var result = Remove_Element.RemoveElement(nums, val);
+            Assert.That(result, Is.EqualTo(expected));
+            // Assert that first k values in nums contains all the numbers in the array except the val
+            for (int i = 0; i < expected; i++)
+            {
+                // Assert that expectedArray[i] is present in nums
+                Assert.That(nums.Take(expected).Contains(expectedArray[i]), Is.True);
+            }
+        }
+    }
+}
---
+++ LeetCode/66-Plus-One.cs
@@ -0,0 +1,82 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public class Plus_One
+    {
+        public int[] PlusOne(int[] digits)
+        {
+            var lastDigit = digits[digits.Length - 1];
+
+            if (lastDigit < 9)
+            {
+                digits[digits.Length - 1] = lastDigit + 1;
+                return digits;
+            }
+
+            if (digits.Length == 1)
+            {
+                return [1, 0];
+            }
+
+            var subarray = new ArraySegment<int>(digits, 0, digits.Length -1).ToArray();
+            return [.. PlusOne(subarray), 0];
+        }
+    }
+
+    // Generate a testing class for this method using NUnit
+    [TestFixture]
+    public class PlusOneTests
+    {
+        private Plus_One solution;
+
+        [SetUp]
+        public void SetUp()
+        {
+            solution = new Plus_One();
+        }
+
+        [Test]
+        public void Test1()
+        {
+            var digits = new int[] { 1, 2, 3 };
+            var expected = new int[] { 1, 2, 4 };
+            var result = solution.PlusOne(digits);
+            Assert.That(result, Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void Test2()
+        {
+            var digits = new int[] { 4, 3, 2, 1 };
+            var expected = new int[] { 4, 3, 2, 2 };
+            var result = solution.PlusOne(digits);
+            Assert.That(result, Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void Test3()
+        {
+            var digits = new int[] { 9 };
+            var expected = new int[] { 1, 0 };
+            var result = solution.PlusOne(digits);
+            Assert.That(result, Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void Test4()
+        {
+            var digits = new int[] { 9, 9 };
+            var expected = new int[] { 1, 0, 0 };
+            var result = solution.PlusOne(digits);
+            Assert.That(result, Is.EqualTo(expected));
+        }
+    }
+
+
+}
